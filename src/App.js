614c4b4184e6c07daa669a1b955f5458125f8b83
Natnael Tezera <nmexodus870@gmail.com>
2022-11-09 11:49:15 -0800

    creating component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import Header from './Header';
+// import Header from './Header';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return React.createElement("h1", {}, "Hello World!!!");
+    // (
+    //   <div className="App">
+    //     <Header/>
+    //   </div>
+    // );
+  }  
 }
 
 export default App;
